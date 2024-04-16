a6d731fa526257725aa8e8c79b39534827bf64d4
SebastianSalas <janiersalas09@gmail.com>
2024-04-15 20:35:03 -0500

    fixes typo errors and adds diego service

--- orquestador/orquestar.py
+++ orquestador/orquestar.py
@@ -8,9 +8,14 @@ app = FastAPI()
 async def orquestar():
     async with httpx.AsyncClient() as client:
         try:
-            respuesta_a = await client.get("http://servicio-sebas-service/servicio-sebas")
-            data_a = respuesta_a.json()
+            respuesta_sebas = await client.get("http://servicio-sebas-service/servicio-sebas")
+            data_sebas = respuesta_sebas.json()
         except httpx.RequestError:
-            data_a = "El servicio de sebas no está disponible"
+            data_sebas = "El servicio de sebas no está disponible"
+        try:
+            respuesta_diego = await client.get("http://servicio-diego-service/servicio-diego")
+            data_diego = respuesta_diego.json()
+        except httpx.RequestError:
+            data_diego = "El servicio de diego no está disponible"
 
-    return {"respuesta_a": data_a}
+    return {"respuesta_sebas": data_sebas, "respuesta_diego": data_diego}
--- serviciosebas/servicio_sebas.py
+++ serviciosebas/servicio_sebas.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get('/servicio-sebas')
 async def servicio_sebas():
-    return {"mensaje": "Hola soy sebastian"}
+    return {"mensaje": "Hola soy sebastian, estudiante de ingeniería y me gusta ir al gimnasio."}
